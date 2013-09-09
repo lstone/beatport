17dbe09d51125ba2f3dfea8662693f75c2d1bb5e
Lawrence Stone <larry@symbolshift.com>
2013-09-09 09:52:41 -0700

    Working on adding most popular calls to Release.

--- lib/beatport/catalog/release.rb
+++ lib/beatport/catalog/release.rb
@@ -1,37 +1,49 @@
 module Beatport
-  module Catalog
-    class Release < Item
-      has_many :artists, Artist
-      has_one :audio_format_fee, AudioFormatFee
-      has_many :genres, Genre
-      has_one :images, Images
-      has_one :label, Label
-      has_one :price, Price
-      has_one :recommendations, Recommendations
-      has_many :tracks, Track
-      has_one :tracks_price, Price
+    module Catalog
+        class Release < Item
+            has_many :artists, Artist
+            has_one :audio_format_fee, AudioFormatFee
+            has_many :genres, Genre
+            has_one :images, Images
+            has_one :label, Label
+            has_one :price, Price
+            has_one :recommendations, Recommendations
+            has_many :tracks, Track
+            has_one :tracks_price, Price
 
-      def discount
-        tracks_price - price
-      end
-    
-      def self.find(*args)
-        Client.retrieve 'releases', Release, *args
-      end
+            def discount
+                tracks_price - price
+            end
 
-      def self.all(options={})
-        Client.retrieve 'releases', Release, options
-      end    
-    
-      def self.featured(*args)
-        Client.retrieve 'featured/releases', Release, *args
-      end
+            def self.find(*args)
+                Client.retrieve 'releases', Release, *args
+            end
 
-      def tracks(options = {})
-        options[:release_id] = id
-        Track.all(options)
-      end
+            def self.all(options={})
+                Client.retrieve 'releases', Release, options
+            end
+
+            def self.featured(*args)
+                Client.retrieve 'featured/releases', Release, *args
+            end
+
+            def self.most_popular(id, options = {})
+                options[:id] = id
+                options[:collection] = true
+                Client.retrieve "most-popular-releases", Release, options
+            end
+
+            def self.most_popular_for_genre(id, options = {})
+                options[:id] = id
+                options[:collection] = true
+                Client.retrieve "most-popular-releases/genre", Release, options
+            end
+
+            def tracks(options = {})
+                options[:release_id] = id
+                Track.all(options)
+            end
+        end
     end
-  end
 end
   
